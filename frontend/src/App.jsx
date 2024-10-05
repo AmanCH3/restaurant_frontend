f014d1fc86222611fba5cb25c90513ba14bc53ce
AmanCH3 <armanachaudhary53@gmail.com>
2024-10-05 02:33:31 -0700

    feat : Hero section design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Herosection  from "./components/herosection";
+import BoookingSection from "./components/bookingsection";
 function App() {
 
   return (
     <div className='bg-white min-h-screen'>
       <Navbar />
       <Herosection/>
+      <BoookingSection/>
 
     </div>
     
---
+++ frontend/src/components/bookingsection.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const BoookingSection = () => {
+  return (
+    <section className="text-center bg-gray-100 py-10">
+      <h2 className="text-2xl font-bold mb-8">Filter by location and Find the Favorite Dish</h2>
+      <div className="flex justify-between space-x-9 items-center pb-2 shadow-sm max-w-4xl mx-auto bg-white p-6 rounded-lg">
+        <select className="bg-white text-black rounded-md ">
+          <option>Select Location</option>
+         
+        </select>
+        <select className="bg-white text-gray-800 rounded-md ">
+          <option>Menu</option>
+         
+        </select>
+        <button className="bg-green-600 text-white px-10 py-2 rounded-md shadow-md hover:border-gray-400">
+          Search
+        </button>
+      </div>
+    </section>
+
+    
+  )
+}
+
+export default BoookingSection
