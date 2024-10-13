17b9b27af330a5217f7b1f1529d6fe450684ab74
AmanCH3 <armanachaudhary53@gmail.com>
2024-10-13 00:32:06 -0700

    Fix in the route section espacially in Login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // import necessary components
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-
-import Login from './page/Login.jsx';
-import SignUp from './page/signup.jsx';
+import Login from './page/Login.jsx' ;
+import SignUp from './page/signup.jsx'; 
 
 const router = createBrowserRouter([
   {
@@ -14,16 +13,16 @@ const router = createBrowserRouter([
   },
   {
     path: '/login',
-    element: <Login />,
-
-    path : '/signup',
-    element : <SignUp/>,
-    
+    element: <Login />, 
+  },
+  {
+    path: '/signup',
+    element: <SignUp />, 
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} /> {/* Use RouterProvider to apply routes */}
+    <RouterProvider router={router} />
   </StrictMode>
 );
