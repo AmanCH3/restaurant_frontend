4f890953d4b193720107dc68e62a7d4e331dfd54
AmanCH3 <armanachaudhary53@gmail.com>
2024-12-27 19:26:22 +0545

    chore: modified components

--- frontend/src/page/aboutus.jsx
+++ frontend/src/page/aboutus.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import aboutus from '../assets/aboutus/Aboutus.png';
-import person1 from '../assets/aboutus/person1.png';
-import person2 from '../assets/aboutus/person2.png';
 import NavBar from '../components/navbar';
 import Footer from '../components/footer';
 
@@ -19,7 +16,7 @@ const Aboutus
 
 
         <div>
-            <img src={aboutus} alt="Logo of the restuarnt" />
+            <img src={"../src/assets/aboutus/aboutus.png"} alt="Logo of the restuarnt" />
         </div>
 
         <div className='flex  items-center justify-center m-20 '>
@@ -43,7 +40,7 @@ const Aboutus
         <div className=' bg-gallery flex flex-row m-20 gap-10 pb-20'>
 
             <div className='flex flex-col items-center space-y-3'>
-            <img src={person1} alt="lady" />
+            <img src={"../src/assets/aboutus/person1.png"} alt="lady" />
             <h1 className='font-semibold'>Shobha Sharma (Persident)</h1>
             <p className='text-center'>Greeting , I'm Shobha Sharma, acting on behalf of the resturant . You 
                 can reach me at (920) 917-9987. We appreciate your patronage .
@@ -51,7 +48,7 @@ const Aboutus
             </div>
 
             <div className='flex flex-col items-center space-y-3 bg-gallery'>
-                <img src={person2} alt="photo of CEO" />
+                <img src={"../src/assets/aboutus/person2.png"} alt="photo of CEO" />
                 <h1 className='font-semibold'>Basudev Adhikair (C.E.O)</h1>
                 <p className='text-center'>I'm Basudev Adhikari , acting on behalf of the CEO of this resturant . You 
                     can reach me via email of missionbda@gmail.com or by phone at (920) 226-178-1786.
